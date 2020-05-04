d24a283cfa024f1d1910aa1f13c6c790b94e2d90
Rookzilla <Andybrown7890@gmail.com>
2020-05-04 13:18:41 +0100

    First Commit

---
+++ src/game/server/hl2/weapon_laser.cpp
@@ -0,0 +1,472 @@
+//========= Copyright Valve Corporation, All rights reserved. ============//
+//
+// Purpose:		Laser - hand gun
+//
+// $NoKeywords: $
+//=============================================================================//
+
+#include "cbase.h"
+#include "npcevent.h"
+#include "basehlcombatweapon.h"
+#include "basecombatcharacter.h"
+#include "ai_basenpc.h"
+#include "player.h"
+#include "gamerules.h"
+#include "in_buttons.h"
+#include "soundent.h"
+#include "game.h"
+#include "vstdlib/random.h"
+	#include "gamestats.h"	
+
+	#include "beam_shared.h"	/* We will use some of the functions declared this later... */
+	#include "ammodef.h"		/* This is needed for the tracing done later */
+
+	// This determines the sprite used by the laser beam
+	#define PHYSCANNON_BEAM_SPRITE "sprites/orangelight1.vmt"
+
+// memdbgon must be the last include file in a .cpp file!!!
+#include "tier0/memdbgon.h"
+
+#define	PISTOL_FASTEST_REFIRE_TIME		0.1f
+#define	PISTOL_FASTEST_DRY_REFIRE_TIME	0.2f
+
+#define	PISTOL_ACCURACY_SHOT_PENALTY_TIME		0.2f	// Applied amount of time each shot adds to the time we must recover from
+#define	PISTOL_ACCURACY_MAXIMUM_PENALTY_TIME	1.5f	// Maximum penalty to deal out
+
+ConVar	laser_use_new_accuracy( "laser_use_new_accuracy", "1" );
+
+//-----------------------------------------------------------------------------
+// CWeaponLaser
+//-----------------------------------------------------------------------------
+
+class CWeaponLaser : public CBaseHLCombatWeapon
+{
+	DECLARE_DATADESC();
+
+public:
+	DECLARE_CLASS( CWeaponLaser, CBaseHLCombatWeapon );
+
+	CWeaponLaser(void);
+
+	DECLARE_SERVERCLASS();
+	
+	void   	DrawBeam( const Vector &startPos, const Vector &endPos, float width );
+	void	DoImpactEffect( trace_t &tr, int nDamageType );
+	void	Precache( void );
+	void	ItemPostFrame( void );
+	void	ItemPreFrame( void );
+	void	ItemBusyFrame( void );
+	void	PrimaryAttack( trace_t &tr, int nDamageType, CBaseCombatCharacter *pOperator );
+	void	AddViewKick( void );
+	void	DryFire( void );
+	void	Operator_HandleAnimEvent( animevent_t *pEvent, CBaseCombatCharacter *pOperator );
+
+	void	UpdatePenaltyTime( void );
+
+	int		CapabilitiesGet( void ) { return bits_CAP_WEAPON_RANGE_ATTACK1; }
+	Activity	GetPrimaryAttackActivity( void );
+
+	virtual bool Reload( void );
+
+	virtual const Vector& GetBulletSpread( void )
+	{		
+		// Handle NPCs first
+		static Vector npcCone = VECTOR_CONE_5DEGREES;
+		if ( GetOwner() && GetOwner()->IsNPC() )
+			return npcCone;
+			
+		static Vector cone;
+
+		if ( laser_use_new_accuracy.GetBool() )
+		{
+			float ramp = RemapValClamped(	m_flAccuracyPenalty, 
+											0.0f, 
+											PISTOL_ACCURACY_MAXIMUM_PENALTY_TIME, 
+											0.0f, 
+											1.0f ); 
+
+			// We lerp from very accurate to inaccurate over time
+			VectorLerp( VECTOR_CONE_1DEGREES, VECTOR_CONE_6DEGREES, ramp, cone );
+		}
+		else
+		{
+			// Old value
+			cone = VECTOR_CONE_4DEGREES;
+		}
+
+		return cone;
+	}
+	
+	virtual int	GetMinBurst() 
+	{ 
+		return 1; 
+	}
+
+	virtual int	GetMaxBurst() 
+	{ 
+		return 3; 
+	}
+
+	virtual float GetFireRate( void ) 
+	{
+		return 0.5f; 
+	}
+
+	DECLARE_ACTTABLE();
+	
+	int	m_nBulletType;
+
+private:
+	float	m_flSoonestPrimaryAttack;
+	float	m_flLastAttackTime;
+	float	m_flAccuracyPenalty;
+	int		m_nNumShotsFired;
+};
+
+
+IMPLEMENT_SERVERCLASS_ST(CWeaponLaser, DT_WeaponLaser)
+END_SEND_TABLE()
+
+LINK_ENTITY_TO_CLASS( weapon_laser, CWeaponLaser );
+PRECACHE_WEAPON_REGISTER( weapon_laser );
+
+BEGIN_DATADESC( CWeaponLaser )
+
+	DEFINE_FIELD( m_flSoonestPrimaryAttack, FIELD_TIME ),
+	DEFINE_FIELD( m_flLastAttackTime,		FIELD_TIME ),
+	DEFINE_FIELD( m_flAccuracyPenalty,		FIELD_FLOAT ), //NOTENOTE: This is NOT tracking game time
+	DEFINE_FIELD( m_nNumShotsFired,			FIELD_INTEGER ),
+	DEFINE_FIELD( m_nBulletType,			FIELD_INTEGER ),
+
+END_DATADESC()
+
+acttable_t	CWeaponLaser::m_acttable[] = 
+{
+	{ ACT_RANGE_ATTACK1,			ACT_RANGE_ATTACK_AR2,			true },
+	{ ACT_RELOAD,					ACT_RELOAD_SMG1,				true },		// FIXME: hook to AR2 unique
+	{ ACT_IDLE,						ACT_IDLE_SMG1,					true },		// FIXME: hook to AR2 unique
+	{ ACT_IDLE_ANGRY,				ACT_IDLE_ANGRY_SMG1,			true },		// FIXME: hook to AR2 unique
+
+	{ ACT_WALK,						ACT_WALK_RIFLE,					true },
+
+// Readiness activities (not aiming)
+	{ ACT_IDLE_RELAXED,				ACT_IDLE_SMG1_RELAXED,			false },//never aims
+	{ ACT_IDLE_STIMULATED,			ACT_IDLE_SMG1_STIMULATED,		false },
+	{ ACT_IDLE_AGITATED,			ACT_IDLE_ANGRY_SMG1,			false },//always aims
+
+	{ ACT_WALK_RELAXED,				ACT_WALK_RIFLE_RELAXED,			false },//never aims
+	{ ACT_WALK_STIMULATED,			ACT_WALK_RIFLE_STIMULATED,		false },
+	{ ACT_WALK_AGITATED,			ACT_WALK_AIM_RIFLE,				false },//always aims
+
+	{ ACT_RUN_RELAXED,				ACT_RUN_RIFLE_RELAXED,			false },//never aims
+	{ ACT_RUN_STIMULATED,			ACT_RUN_RIFLE_STIMULATED,		false },
+	{ ACT_RUN_AGITATED,				ACT_RUN_AIM_RIFLE,				false },//always aims
+
+// Readiness activities (aiming)
+	{ ACT_IDLE_AIM_RELAXED,			ACT_IDLE_SMG1_RELAXED,			false },//never aims	
+	{ ACT_IDLE_AIM_STIMULATED,		ACT_IDLE_AIM_RIFLE_STIMULATED,	false },
+	{ ACT_IDLE_AIM_AGITATED,		ACT_IDLE_ANGRY_SMG1,			false },//always aims
+
+	{ ACT_WALK_AIM_RELAXED,			ACT_WALK_RIFLE_RELAXED,			false },//never aims
+	{ ACT_WALK_AIM_STIMULATED,		ACT_WALK_AIM_RIFLE_STIMULATED,	false },
+	{ ACT_WALK_AIM_AGITATED,		ACT_WALK_AIM_RIFLE,				false },//always aims
+
+	{ ACT_RUN_AIM_RELAXED,			ACT_RUN_RIFLE_RELAXED,			false },//never aims
+	{ ACT_RUN_AIM_STIMULATED,		ACT_RUN_AIM_RIFLE_STIMULATED,	false },
+	{ ACT_RUN_AIM_AGITATED,			ACT_RUN_AIM_RIFLE,				false },//always aims
+//End readiness activities
+
+	{ ACT_WALK_AIM,					ACT_WALK_AIM_RIFLE,				true },
+	{ ACT_WALK_CROUCH,				ACT_WALK_CROUCH_RIFLE,			true },
+	{ ACT_WALK_CROUCH_AIM,			ACT_WALK_CROUCH_AIM_RIFLE,		true },
+	{ ACT_RUN,						ACT_RUN_RIFLE,					true },
+	{ ACT_RUN_AIM,					ACT_RUN_AIM_RIFLE,				true },
+	{ ACT_RUN_CROUCH,				ACT_RUN_CROUCH_RIFLE,			true },
+	{ ACT_RUN_CROUCH_AIM,			ACT_RUN_CROUCH_AIM_RIFLE,		true },
+	{ ACT_GESTURE_RANGE_ATTACK1,	ACT_GESTURE_RANGE_ATTACK_AR2,	false },
+	{ ACT_COVER_LOW,				ACT_COVER_SMG1_LOW,				false },		// FIXME: hook to AR2 unique
+	{ ACT_RANGE_AIM_LOW,			ACT_RANGE_AIM_AR2_LOW,			false },
+	{ ACT_RANGE_ATTACK1_LOW,		ACT_RANGE_ATTACK_SMG1_LOW,		true },		// FIXME: hook to AR2 unique
+	{ ACT_RELOAD_LOW,				ACT_RELOAD_SMG1_LOW,			false },
+	{ ACT_GESTURE_RELOAD,			ACT_GESTURE_RELOAD_SMG1,		true },
+//	{ ACT_RANGE_ATTACK2, ACT_RANGE_ATTACK_AR2_GRENADE, true },
+};
+
+
+IMPLEMENT_ACTTABLE( CWeaponLaser );
+
+//-----------------------------------------------------------------------------
+// Purpose: Constructor
+//-----------------------------------------------------------------------------
+CWeaponLaser::CWeaponLaser( void )
+{
+	m_flSoonestPrimaryAttack = gpGlobals->curtime;
+	m_nBulletType = -1;
+	m_flAccuracyPenalty = 0.0f;
+
+	m_fMinRange1		= 24;
+	m_fMaxRange1		= 1500;
+	m_fMinRange2		= 24;
+	m_fMaxRange2		= 200;
+
+	m_bFiresUnderwater	= true;
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+//-----------------------------------------------------------------------------
+void CWeaponLaser::Precache( void )
+{
+	BaseClass::Precache();
+}
+
+//-----------------------------------------------------------------------------
+// Purpose:
+// Input  :
+// Output :
+//-----------------------------------------------------------------------------
+void CWeaponLaser::Operator_HandleAnimEvent( animevent_t *pEvent, CBaseCombatCharacter *pOperator )
+{
+	switch( pEvent->event )
+	{
+		case EVENT_WEAPON_PISTOL_FIRE:
+		{
+			Vector vecShootOrigin, vecShootDir;
+			vecShootOrigin = pOperator->Weapon_ShootPosition();
+
+			CAI_BaseNPC *npc = pOperator->MyNPCPointer();
+			ASSERT( npc != NULL );
+
+			vecShootDir = npc->GetActualShootTrajectory( vecShootOrigin );
+
+			CSoundEnt::InsertSound( SOUND_COMBAT|SOUND_CONTEXT_GUNFIRE, pOperator->GetAbsOrigin(), SOUNDENT_VOLUME_PISTOL, 0.2, pOperator, SOUNDENT_CHANNEL_WEAPON, pOperator->GetEnemy() );
+
+			WeaponSound( SINGLE_NPC );
+			pOperator->FireBullets( 1, vecShootOrigin, vecShootDir, VECTOR_CONE_PRECALCULATED, MAX_TRACE_LENGTH, m_iPrimaryAmmoType, 2 );
+			pOperator->DoMuzzleFlash();
+			m_iClip1 = m_iClip1 - 1;
+		}
+		break;
+		default:
+			BaseClass::Operator_HandleAnimEvent( pEvent, pOperator );
+			break;
+	}
+}
+
+//-----------------------------------------------------------------------------
+// Purpose:
+//-----------------------------------------------------------------------------
+void CWeaponLaser::DryFire( void )
+{
+	WeaponSound( EMPTY );
+	SendWeaponAnim( ACT_VM_DRYFIRE );
+	
+	m_flSoonestPrimaryAttack	= gpGlobals->curtime + PISTOL_FASTEST_DRY_REFIRE_TIME;
+	m_flNextPrimaryAttack		= gpGlobals->curtime + SequenceDuration();
+}
+
+//-----------------------------------------------------------------------------
+// Purpose:
+//-----------------------------------------------------------------------------
+void CWeaponLaser::PrimaryAttack( trace_t &tr, int nDamageType, CBaseCombatCharacter *pOperator )
+{
+	if ( ( gpGlobals->curtime - m_flLastAttackTime ) > 0.5f )
+	{
+		m_nNumShotsFired = 0;
+	}
+	else
+	{
+		m_nNumShotsFired++;
+	}
+
+	m_flLastAttackTime = gpGlobals->curtime;
+	m_flSoonestPrimaryAttack = gpGlobals->curtime + PISTOL_FASTEST_REFIRE_TIME;
+	CSoundEnt::InsertSound( SOUND_COMBAT, GetAbsOrigin(), SOUNDENT_VOLUME_PISTOL, 0.2, GetOwner() );
+
+	CBasePlayer *pOwner = ToBasePlayer( GetOwner() );
+
+	if( pOwner )
+	{
+		// Each time the player fires the laser, reset the view punch. This prevents
+		// the aim from 'drifting off' when the player fires very quickly. This may
+		// not be the ideal way to achieve this, but it's cheap and it works, which is
+		// great for a feature we're evaluating. (sjb)
+		pOwner->ViewPunchReset();
+	}
+
+	BaseClass::PrimaryAttack();
+
+	// Add an accuracy penalty which can move past our maximum penalty time if we're really spastic
+	m_flAccuracyPenalty += PISTOL_ACCURACY_SHOT_PENALTY_TIME;
+
+	Vector vecShootOrigin, vecShootDir;
+	vecShootOrigin = pOperator->Weapon_ShootPosition();
+	DrawBeam( vecShootOrigin, tr.endpos, 15.5 );
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+//-----------------------------------------------------------------------------
+void CWeaponLaser::UpdatePenaltyTime( void )
+{
+	CBasePlayer *pOwner = ToBasePlayer( GetOwner() );
+
+	if ( pOwner == NULL )
+		return;
+
+	// Check our penalty time decay
+	if ( ( ( pOwner->m_nButtons & IN_ATTACK ) == false ) && ( m_flSoonestPrimaryAttack < gpGlobals->curtime ) )
+	{
+		m_flAccuracyPenalty -= gpGlobals->frametime;
+		m_flAccuracyPenalty = clamp( m_flAccuracyPenalty, 0.0f, PISTOL_ACCURACY_MAXIMUM_PENALTY_TIME );
+	}
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+//-----------------------------------------------------------------------------
+void CWeaponLaser::ItemPreFrame( void )
+{
+	UpdatePenaltyTime();
+
+	BaseClass::ItemPreFrame();
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+//-----------------------------------------------------------------------------
+void CWeaponLaser::ItemBusyFrame( void )
+{
+	UpdatePenaltyTime();
+
+	BaseClass::ItemBusyFrame();
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: Allows firing as fast as button is pressed
+//-----------------------------------------------------------------------------
+void CWeaponLaser::ItemPostFrame( void )
+{
+	BaseClass::ItemPostFrame();
+
+	if ( m_bInReload )
+		return;
+	
+	CBasePlayer *pOwner = ToBasePlayer( GetOwner() );
+
+	if ( pOwner == NULL )
+		return;
+
+	//Allow a refire as fast as the player can click
+	if ( ( ( pOwner->m_nButtons & IN_ATTACK ) == false ) && ( m_flSoonestPrimaryAttack < gpGlobals->curtime ) )
+	{
+		m_flNextPrimaryAttack = gpGlobals->curtime - 0.1f;
+	}
+	else if ( ( pOwner->m_nButtons & IN_ATTACK ) && ( m_flNextPrimaryAttack < gpGlobals->curtime ) && ( m_iClip1 <= 0 ) )
+	{
+		DryFire();
+	}
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+// Output : int
+//-----------------------------------------------------------------------------
+Activity CWeaponLaser::GetPrimaryAttackActivity( void )
+{
+	if ( m_nNumShotsFired < 1 )
+		return ACT_VM_PRIMARYATTACK;
+
+	if ( m_nNumShotsFired < 2 )
+		return ACT_VM_RECOIL1;
+
+	if ( m_nNumShotsFired < 3 )
+		return ACT_VM_RECOIL2;
+
+	return ACT_VM_RECOIL3;
+}
+
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+bool CWeaponLaser::Reload( void )
+{
+	bool fRet = DefaultReload( GetMaxClip1(), GetMaxClip2(), ACT_VM_RELOAD );
+	if ( fRet )
+	{
+		WeaponSound( RELOAD );
+		m_flAccuracyPenalty = 0.0f;
+	}
+	return fRet;
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+//-----------------------------------------------------------------------------
+void CWeaponLaser::AddViewKick( void )
+{
+	CBasePlayer *pPlayer  = ToBasePlayer( GetOwner() );
+	
+	if ( pPlayer == NULL )
+		return;
+
+	QAngle	viewPunch;
+
+	viewPunch.x = random->RandomFloat( 0.25f, 0.5f );
+	viewPunch.y = random->RandomFloat( -.6f, .6f );
+	viewPunch.z = 0.0f;
+
+	//Add it to the view punch
+	pPlayer->ViewPunch( viewPunch );
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+// Input  : &startPos - where the beam should begin
+//          &endPos - where the beam should end
+//          width - what the diameter of the beam should be (units?)
+//-----------------------------------------------------------------------------
+void CWeaponLaser::DrawBeam(const Vector &startPos, const Vector &endPos, float width)
+{
+	//Tracer down the middle
+	UTIL_Tracer(startPos, endPos, 0, TRACER_DONT_USE_ATTACHMENT, 6500, false, "GaussTracer");
+
+	//Draw the main beam shaft
+	CBeam *pBeam = CBeam::BeamCreate(PHYSCANNON_BEAM_SPRITE, 15.5);
+
+	// It starts at startPos
+	pBeam->SetStartPos(startPos);
+
+	// This sets up some things that the beam uses to figure out where
+	// it should start and end
+	pBeam->PointEntInit(endPos, this);
+
+	// This makes it so that the laser appears to come from the muzzle of the pistol
+	pBeam->SetEndAttachment(LookupAttachment("Muzzle"));
+	pBeam->SetWidth(width);
+	//	pBeam->SetEndWidth( 0.05f );
+
+	// Higher brightness means less transparent
+	pBeam->SetBrightness(255);
+	pBeam->SetColor(255, 185 + random->RandomInt(-16, 16), 40);
+	pBeam->RelinkBeam();
+
+	// The beam should only exist for a very short time
+	pBeam->LiveForTime(0.1f);
+}
+//-----------------------------------------------------------------------------
+// Purpose: 
+// Input  : &tr - used to figure out where to do the effect
+//          nDamageType - ???
+//-----------------------------------------------------------------------------
+void CWeaponLaser::DoImpactEffect(trace_t &tr, int nDamageType)
+{
+	//Draw our beam
+	DrawBeam(tr.startpos, tr.endpos, 15.5);
+	if ((tr.surface.flags & SURF_SKY) == false)
+	{
+		CPVSFilter filter(tr.endpos);
+		te->GaussExplosion(filter, 0.0f, tr.endpos, tr.plane.normal, 0);
+		m_nBulletType = GetAmmoDef()->Index("GaussEnergy");
+		UTIL_ImpactTrace(&tr, m_nBulletType);
+	}
+}
